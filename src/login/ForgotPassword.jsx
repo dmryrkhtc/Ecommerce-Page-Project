1a3be74391fdeab2085ca29b5b799267407d4739
dmryrkhtc <demiryurekhatice@outlook.com>
2024-08-12 04:04:39 +0300

    I forgot to do ctrl+s,saved them again

---
+++ src/login/ForgotPassword.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "./ForgotPassword.css";
+import { Link } from "react-router-dom";
+
+const ForgotPassword = () => {
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setMessage("The password reset link has been sent to your e-mail address.");
+  };
+
+  return (
+    <div className="forgot-password-page">
+      <h1>Forgot my password</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="email">E-mail</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Send</button>
+      </form>
+      {message && <p className="message">{message}</p>}
+      <p>
+        To return to<Link to="/login">Return to the Home Page</Link>
+      </p>
+    </div>
+  );
+};
+
+export default ForgotPassword;
